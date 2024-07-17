cdabfdec5265123416cf6d3bfb3970956cc4167b
Moein-ShiSheBor <shisheborm79@gmail.com>
2024-07-18 02:02:54 +0330

    Initial Commit

---
+++ XO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XO
+//
+//  Created by moein shishebor on 2024-07-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XO/XOApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOApp.swift
+//  XO
+//
+//  Created by moein shishebor on 2024-07-18.
+//
+
+import SwiftUI
+
+@main
+struct XOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
